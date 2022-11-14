bae55962b1d45c0c4d251bfea2eca631064c0d37
HackerInTokyo <flare.kagura@gmail.com>
2022-11-14 21:42:12 +0900

    create

---
+++ DB/7/create_players_table.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sqlite3
+import random
+
+dbname = 'cit-7.db'
+# 1.データベースに接続
+conn = sqlite3.connect(dbname)
+
+# 2.sqliteを操作するカーソルオブジェクトを作成
+cur = conn.cursor()
+
+comstr = "create table player (person_id INTEGER PRIMARY KEY AUTOINCREMENT, fname VARCHAR(20), lname VARCHAR(20), points INTEGER, rank VARCHAR(20));"
+
+# 3.テーブルに人名データを登録する
+cur.execute(comstr)
+
+
+
+
+# 4.データベースにデータをコミット
+conn.commit()
+
+# 5.データベースの接続を切断
+cur.close()
+conn.close()
