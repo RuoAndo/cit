9bc68b6c600665d0bb1ae1c4d05757a9f2e21865
RuoAndo <ando.ruo@gmail.com>
2022-11-25 10:45:43 +0900

    9

---
+++ DB/9/README.md
@@ -0,0 +1,8 @@
+0. ƒe[ƒuƒ‹‚ðì¬‚·‚é
+
+(base) PS C:\Users\flare\OneDrive\cit\DB\9> python .\create_players_table.py
+(base) PS C:\Users\flare\OneDrive\cit\DB\9> python .\create_characters_table.py
+(base) PS C:\Users\flare\OneDrive\cit\DB\9> python .\create_events_table.py
+(base) PS C:\Users\flare\OneDrive\cit\DB\9> python .\show_tables.py
+
+
---
+++ DB/9/create_characters_table.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sqlite3
+import random
+
+dbname = 'cit-7.db'
+# 1.データベースに接続
+conn = sqlite3.connect(dbname)
+
+# 2.sqliteを操作するカーソルオブジェクトを作成
+cur = conn.cursor()
+
+try:
+	comstr1 = "drop table character;"
+	cur.execute(comstr1)
+
+except:
+	pass
+
+comstr2 = "create table character (person_id INTEGER, character_id INTEGER, character_name VARCHAR(20), HP INTERGER, MP INTEGER, EXP INTEGER);"
+
+
+
+# 3.テーブルに人名データを登録する
+cur.execute(comstr2)
+
+# 4.データベースにデータをコミット
+conn.commit()
+
+# 5.データベースの接続を切断
+cur.close()
+conn.close()
