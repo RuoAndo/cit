1141e9cc4737bb2a0e8b406af7bdc68764804162
RuoAndo <ando.ruo@gmail.com>
2022-12-07 23:36:18 +0900

    py

---
+++ DB/9/show_tables_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sqlite3
+import random
+
+dbname = 'cit-7.db'
+# 1.データベースに接続
+conn = sqlite3.connect(dbname)
+
+# 2.sqliteを操作するカーソルオブジェクトを作成
+cur = conn.cursor()
+
+cur.execute("select * from player")
+for row in cur.fetchall():
+    print(row)
+
+cur.execute("select * from character")
+for row in cur.fetchall():
+    print(row)
+
+cur.close()
+conn.close()
